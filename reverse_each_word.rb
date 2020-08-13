f3d8b4cfc28a7c350a8933f9e29327ea5a35ec4c
Ariba Khan <akhan41@binghamton.edu>
2020-08-13 21:36:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   sentence_array = sentence.split('')
   reverse_sentence = ""
   sentence_array.each{|c|
-    reverse_sentence = reverse_sentence + c
+    reverse_sentence = c + reverse_sentence
   }
   reverse_sentence
 end
