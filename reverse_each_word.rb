342dd9022019b896a3d2b168e5c62b560904c8d5
Ariba Khan <akhan41@binghamton.edu>
2020-08-13 21:55:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,3 +9,15 @@ def reverse_each_word(sentence)
   }
   reverse_sentence.delete_suffix(" ")
 end
+
+def reverse_each_word(sentence)
+  sentence_array = sentence.split(' ')
+  reverse_sentence = ""
+  sentence_array.collect{|c|
+    c_array = c.split('')
+    reverse_word = ""
+    c_array.collect{|d| reverse_word = d + reverse_word}
+    reverse_sentence = reverse_sentence  + reverse_word + " "
+  }
+  reverse_sentence.delete_suffix(" ")
+end
