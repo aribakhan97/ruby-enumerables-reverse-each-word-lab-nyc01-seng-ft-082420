95541b62a9d4b794c1241230bd114bda3c6e8079
Ariba Khan <akhan41@binghamton.edu>
2020-08-13 21:44:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word(sentence)
   sentence_array.each{|c|
     c_array = c.split('')
     reverse_word = ""
-    c_array.each{|d| + }
+    c_array.each{|d| reverse_word = d + reverse_word}
     reverse_sentence = c + reverse_sentence
   }
   reverse_sentence
