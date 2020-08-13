8156286833795a0d2ea834bb9fd3fa14e6744e57
Ariba Khan <akhan41@binghamton.edu>
2020-08-13 21:41:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,9 @@
 def reverse_each_word(sentence)
-  sentence_array = sentence.split('')
+  sentence_array = sentence.split(' ')
   reverse_sentence = ""
   sentence_array.each{|c|
+    c_array = c.split('')
+    c_array.each{|d|}
     reverse_sentence = c + reverse_sentence
   }
   reverse_sentence
