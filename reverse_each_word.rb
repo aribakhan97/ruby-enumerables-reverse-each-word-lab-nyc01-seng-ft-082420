4c4c65933dd640b0b54e807caddbc78d168eb125
Ariba Khan <akhan41@binghamton.edu>
2020-08-13 21:56:35 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,2 +1,23 @@
 def reverse_each_word(sentence)
-  
+  sentence_array = sentence.split(' ')
+  reverse_sentence = ""
+  sentence_array.each{|c|
+    c_array = c.split('')
+    reverse_word = ""
+    c_array.each{|d| reverse_word = d + reverse_word}
+    reverse_sentence = reverse_sentence  + reverse_word + " "
+  }
+  reverse_sentence.delete_suffix(" ")
+end
+
+def reverse_each_word(sentence)
+  sentence_array = sentence.split(' ')
+  reverse_sentence = ""
+  sentence_array.collect{|c|
+    c_array = c.split('')
+    reverse_word = ""
+    c_array.each{|d| reverse_word = d + reverse_word}
+    reverse_sentence = reverse_sentence  + reverse_word + " "
+  }
+  reverse_sentence.delete_suffix(" ")
+end
